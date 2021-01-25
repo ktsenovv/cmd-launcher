a46386b58f596699a978d35850bc546c862d7136
Kristian Cenow <40594589+kcenow@users.noreply.github.com>
2021-01-25 14:58:37 +0200

    Release

---
+++ source.bat
@@ -0,0 +1,22 @@
+@echo off
+rem [ SETTINGS ]==========================================================
+set AppName=CMD Launcher
+set AppAuthor=kcenow
+set AppVersion=v1.0
+set AppWeb=https://kcenow.com
+set AppNameNum=1
+rem ======================================================================
+
+rem [REGISTER]============================================================
+for /f "tokens=%AppNameNum% delims=_" %%i in ("%~n0") do set ProgramName=%%i
+title %AppName% [%ProgramName%.exe]
+rem ======================================================================
+
+rem [ FUNCTION ]==========================================================
+:FUNC_CMD
+set /p cmd="%ProgramName%>" 
+%ProgramName% %cmd%
+set "cmd="
+echo.
+goto FUNC_CMD
+rem ======================================================================
